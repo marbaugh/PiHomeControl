f8238cd712cd53240be69b332a4299aac32ac615
marbaugh <marbaugh@gmail.com>
2014-03-18 10:56:24 -0400

    Import and Syntax Errors Fixes

--- tests/motorTest.py
+++ tests/motorTest.py
@@ -1,17 +1,17 @@
-from homeautomation import Motor
+from homeautomation.homeautomation import Motor
 
 def motor_forward_test():
 	motor = Motor()
 	print "Moving stepper motor forward"
-    motor.forward(5)
+	motor.forward(5)
 
 def motor_reverse_test():
-	 print "Moving stepper in reverse"
-	 motor.reverse(5)
+	print "Moving stepper in reverse"
+	motor.reverse(5)
 
 def main():
 	motor_forward_test()
 	motor_reverse_test()
 
 if __name__ == '__main__':
-    main()
+	main()
