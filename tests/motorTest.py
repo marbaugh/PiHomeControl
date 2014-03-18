c0d1c414c8ddb8131e64447b04a7bebb7d7a16b3
marbaugh <marbaugh@gmail.com>
2014-03-18 18:37:00 -0400

    Setting up motor test as 'nosetest'

--- tests/motorTest.py
+++ tests/motorTest.py
@@ -1,17 +1,11 @@
 from homecontrol.automation import Motor
 
-def motor_forward_test(motor):
+def test_motor_forward(motor):
+	motor = Motor()
 	print "Moving stepper motor forward"
 	motor.forward(5)
 
-def motor_reverse_test(motor):
+def test_motor_reverse(motor):
+	motor = Motor()
 	print "Moving stepper in reverse"
 	motor.reverse(5)
-
-def main():
-	motor = Motor()
-	motor_forward_test(motor)
-	motor_reverse_test(motor)
-
-if __name__ == '__main__':
-	main()
