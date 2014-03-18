5426524001f6b6b58d05187a39664be556f448f6
marbaugh <marbaugh@gmail.com>
2014-03-18 19:05:24 -0400

    Adding setup and teardown for nose test

--- tests/motorTest.py
+++ tests/motorTest.py
@@ -1,12 +1,20 @@
 from homecontrol.automation import Motor
 
+def setup_func():
+	motor = Motor()
+
+def teardown_func():
+	motor.cleanup()
+
+@with_setup(setup_func, teardown_func)
 def test_motor_forward():
 	motor = Motor()
 	print "Moving stepper motor forward"
 	motor.forward(5)
 	motor.cleanup()
 	pass
-
+	
+@with_setup(setup_func, teardown_func)
 def test_motor_reverse():
 	motor = Motor()
 	print "Moving stepper in reverse"
