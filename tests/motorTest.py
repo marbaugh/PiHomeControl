50b3a274ebc9aaf4fbc89eaeef6ac22bc0539841
marbaugh <marbaugh@gmail.com>
2014-03-18 18:41:37 -0400

    Adding cleanup to the test methods

--- tests/motorTest.py
+++ tests/motorTest.py
@@ -4,10 +4,12 @@ def test_motor_forward():
 	motor = Motor()
 	print "Moving stepper motor forward"
 	motor.forward(5)
+	motor.cleanup()
 	pass
 
 def test_motor_reverse():
 	motor = Motor()
 	print "Moving stepper in reverse"
 	motor.reverse(5)
+	motor.cleanup()
 	pass
