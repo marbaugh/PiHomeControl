f5c8ade3df7d2c26425101ddfadc88338c8d01e7
marbaugh <marbaugh@gmail.com>
2014-03-18 18:04:42 -0400

    Updating setup to reflect name changes to the package

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from setuptools import setup
 
-setup(name='homeautomation',
+setup(name='homecontrol',
       version='1.0',
       description='Home Automation Utilities for Raspberry Pi',
       author='Matthew Arbaugh',
       author_email='marbaugh@gmail.com',
       url='https://github.com/marbaugh/stepperMotor',
       package_dir = {'': 'src'},
-      packages=['homeautomation'],
+      packages=['homecontrol'],
       install_requires=['RPi.GPIO'],
      )
