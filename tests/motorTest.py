5444ea682036c6112a5bc41ba2ee85a6c11f73ea
marbaugh <marbaugh@gmail.com>
2014-03-18 18:19:55 -0400

    Updated test to pass motor instance

--- tests/motorTest.py
+++ tests/motorTest.py
@@ -1,17 +1,17 @@
 from homecontrol.automation import Motor
 
-def motor_forward_test():
-	motor = Motor()
+def motor_forward_test(self, motor):
 	print "Moving stepper motor forward"
 	motor.forward(5)
 
-def motor_reverse_test():
+def motor_reverse_test(self, motor):
 	print "Moving stepper in reverse"
 	motor.reverse(5)
 
 def main():
-	motor_forward_test()
-	motor_reverse_test()
+    motor = Motor()
+	motor_forward_test(motor)
+	motor_reverse_test(motor)
 
 if __name__ == '__main__':
 	main()
