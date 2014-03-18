d0b9c1e938ee16482d102a582af650dbba6c98b1
marbaugh <marbaugh@gmail.com>
2014-03-18 18:38:04 -0400

    Removed unused variable from method

--- tests/motorTest.py
+++ tests/motorTest.py
@@ -1,11 +1,13 @@
 from homecontrol.automation import Motor
 
-def test_motor_forward(motor):
+def test_motor_forward():
 	motor = Motor()
 	print "Moving stepper motor forward"
 	motor.forward(5)
+	pass
 
-def test_motor_reverse(motor):
+def test_motor_reverse():
 	motor = Motor()
 	print "Moving stepper in reverse"
 	motor.reverse(5)
+	pass
