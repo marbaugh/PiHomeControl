e7620f05bd69cc5ef3c1813926fab3b153b1ad45
marbaugh <marbaugh@gmail.com>
2014-04-07 21:37:33 -0400

    Added init and bin folders
    Added init files for publishing door sensor and motion sensor status.
    Also added the python scripts used by the init files.

---
+++ bin/doorSensorPub.py
@@ -0,0 +1,20 @@
+
+from homecontrol.automation import DoorSensor
+import sys
+import time
+import zmq
+
+def pub_door_sensor_status():
+	port = "5556"
+	topic = 10001
+	context = zmq.Context()
+	socket = context.socket(zmq.PUB)
+	socket.bind("tcp://*:%s" % port)
+	while True:
+		messagedata = DoorSensor().status
+		print "%d %d" % (topic, messagedata)
+		socket.send("%d %d" % (topic, messagedata))
+		time.sleep(10)
+
+if __name__ == "__main__":
+            pub_door_sensor_status()
---
+++ bin/motionSensorPub.py
@@ -0,0 +1,20 @@
+
+from homecontrol.automation import MotionSensor
+import sys
+import time
+import zmq
+
+def pub_motion_sensor_status():
+	port = "5556"
+	topic = 10000
+	context = zmq.Context()
+	socket = context.socket(zmq.PUB)
+	socket.bind("tcp://*:%s" % port)
+	while True:
+		messagedata = MotionSensor().status
+		print "%d %d" % (topic, messagedata)
+		socket.send("%d %d" % (topic, messagedata))
+		time.sleep(10)
+
+if __name__ == "__main__":
+            pub_motion_sensor_status()
