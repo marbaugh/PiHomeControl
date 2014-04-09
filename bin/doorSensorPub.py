5efcc88133f60283c97e3359626694b7206e4851
marbaugh <marbaugh@gmail.com>
2014-04-09 08:13:41 -0400

    Sleep after pub
    Sleep for 15 seconds in-between publishing.

--- bin/doorSensorPub.py
+++ bin/doorSensorPub.py
@@ -13,11 +13,11 @@ def pub_door_sensor_status():
     socket.bind("tcp://*:%s" % port)
     # Allow clients to connect before sending data
     sleep(10)
-    #while True:
-    messagedata = DoorSensor().status()
-    print "{0} {1}".format(topic, messagedata)
-    socket.send("{0} {1}".format(topic, messagedata))
-    #time.sleep(10)
+    while True:
+        messagedata = DoorSensor().status()
+        print "{0} {1}".format(topic, messagedata)
+        socket.send("{0} {1}".format(topic, messagedata))
+        time.sleep(15)
 
 if __name__ == "__main__":
     pub_door_sensor_status()
