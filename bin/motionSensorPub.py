9738212f62ed6895f143204e472011e367ea405d
marbaugh <marbaugh@gmail.com>
2014-04-09 00:53:48 +0000

    Troubleshooting zmq

--- bin/motionSensorPub.py
+++ bin/motionSensorPub.py
@@ -1,19 +1,22 @@
+#!/usr/bin/env python
+
 from homecontrol.automation import MotionSensor
 import sys
 import time
 import zmq
 
 def pub_motion_sensor_status():
-	port = "5556"
-	topic = "motion"
-	context = zmq.Context()
-	socket = context.socket(zmq.PUB)
-	socket.bind("tcp://*:%s" % port)
-	while True:
-		messagedata = MotionSensor().status
-		print "%d %d" % (topic, messagedata)
-		socket.send("%d %d" % (topic, messagedata))
-		time.sleep(10)
+     port = "5556"
+     topic = "motion"
+     context = zmq.Context()
+     socket = context.socket(zmq.PUB)
+     socket.bind("tcp://*:{0}".format(port))
+#     while True:
+     messagedata = MotionSensor().status()
+     print "{0} {1}".format(topic, messagedata)
+     socket.send("{0} {1}".format(topic, messagedata))
+
+    # time.sleep(10)
 
 if __name__ == "__main__":
-	pub_motion_sensor_status()
+     pub_motion_sensor_status()
