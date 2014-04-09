620607edaa3739e5232d12679c4e65c991334c41
marbaugh <marbaugh@gmail.com>
2014-04-09 00:52:41 +0000

    Troubleshooting zmq

--- bin/doorSensorPub.py
+++ bin/doorSensorPub.py
@@ -1,19 +1,21 @@
+#!/usr/bin/env python
+
 from homecontrol.automation import DoorSensor
 import sys
 import time
 import zmq
 
 def pub_door_sensor_status():
-	port = "5556"
-	topic = "door"
-	context = zmq.Context()
-	socket = context.socket(zmq.PUB)
-	socket.bind("tcp://*:%s" % port)
-	while True:
-		messagedata = DoorSensor().status
-		print "%d %d" % (topic, messagedata)
-		socket.send("%d %d" % (topic, messagedata))
-		time.sleep(10)
+    port = "5556"
+    topic = "door"
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind("tcp://*:%s" % port)
+	#while True:
+    messagedata = DoorSensor().status()
+    print "{0} {1}".format(topic, messagedata)
+    socket.send("{0} {1}".format(topic, messagedata))
+    #time.sleep(10)
 
 if __name__ == "__main__":
-	pub_door_sensor_status()
+    pub_door_sensor_status()
