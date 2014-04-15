32a8a1ada1cb9176854ceb8579997f356afb4f67
marbaugh <marbaugh@gmail.com>
2014-04-15 14:34:28 -0400

    Updated to use event detect instead of zmq

--- bin/motionSensorPub.py
+++ bin/motionSensorPub.py
@@ -3,21 +3,15 @@
 from homecontrol.automation import MotionSensor
 import sys
 import time
-import zmq
+
+
+def MOTION():
+     print "Motion Detected!"
 
 def pub_motion_sensor_status():
-     port = "5556"
-     topic = "motion"
-     context = zmq.Context()
-     socket = context.socket(zmq.PUB)
-     socket.bind("tcp://*:{0}".format(port))
-     # Allow clients to connect before sending data
-     time.sleep(10)
-     while True:
-         messagedata = MotionSensor().status()
-         print "{0} {1}".format(topic, messagedata)
-         socket.send("{0} {1}".format(topic, messagedata))
-         time.sleep(15)
+     MotionSensor().event_detect(MOTION)
+     while 1:
+          time.sleep(100)
 
 if __name__ == "__main__":
      pub_motion_sensor_status()
